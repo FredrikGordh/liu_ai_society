2f50108d7453952ddb50e886209ba60104d6998f
Fredrik Riseby <fredrik.gordh@weknowit.se>
2023-11-28 21:22:08 +0100

    adding some copy content

--- src/components/om-oss/om-oss.js
+++ src/components/om-oss/om-oss.js
@@ -11,28 +11,31 @@ const Om_oss = () => {
             <div class="content-below">
            
                 <div class="paragraph-1">
-                    <h3>Where does it come from?</h3>
-                     
+                    <h3>Who we are</h3>
                     <p>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-                        literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-                        looked up one of the more obscure Latin words,
-                        consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-                        This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    We are a student association at Linköping University devoted to the exploration, 
+                    research and continuous learning of artificial intelligence. We cultivate connections with both 
+                    industry and academia through networking events, educational lectures, workshops and research projects.
+                     We serve as a platform for members and partners to network, build professional relationships, and share knowledge.
                     </p>
                 </div>
 
                 <div class="paragraph-2">
-                    <h3>  Where can I get some?</h3>
+                    <h3>Vision</h3>
                     <p>
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, 
-                        or randomised words which don't look even slightly believable.
-                        If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
-                        All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
-                        It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.
-                        The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
+                    We are committed to organizing events that bridge the gap between students and the spheres of industry and academia. 
+                    Through workshops, seminars, lectures and collaborative projects, we seek to broaden the AI and machine learning knowledge 
+                    base of our student body, and to provide a clear view of potential career paths in these fields. The society is also meant to be a 
+                    platform for engaged and ambitious students to garner internships or research opportunities. 
+
+                    </p>
+                </div>
+
+                <div class="paragraph-3">
+                    <h3>Founding Partners</h3>
+                    <p>
+                    
+
                     </p>
                 </div>
 
